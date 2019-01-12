06d9041e2c30356531e3f0540050a6bb0d45107f
Daniel Briffa <daniel.briffa@bit8.com>
2019-01-12 22:14:47 +0100

    further tests

---
+++ function_return.php
@@ -0,0 +1,30 @@
+<?php
+//Enforce Return Type
+
+
+function add($number1, $number2) : int {
+    return $number1 + $number2;    
+    //return (int)'qwerty'; //this does not throw error !
+}
+
+echo add(1,2).'<br>'; // this will return : 3
+
+
+class animal{
+    public $legs;
+    public $length;
+    public $height;
+}
+
+class dog extends animal{
+    function __construct(){
+        $this->legs = 4;        
+    }    
+}
+
+function animal_type(animal $animal) : int {
+    return $animal->legs;
+}
+echo animal_type(new dog()).'<br>'; //this will return : 4
+
+?>
