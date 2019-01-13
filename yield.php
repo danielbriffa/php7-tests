b6bf7c1d2fd4f9bfdace7ac1ec53f04a79f72ee7
Daniel Briffa <daniel.briffa@bit8.com>
2019-01-13 20:46:50 +0100

    Increasing memory check time

--- yield.php
+++ yield.php
@@ -17,7 +17,7 @@ function loop_value(){
     $test_array = [];
     for ($i=0; $i <= 999999; $i++){
         $test_array[] = $i;
-        if ($i % 20000 === 0) echo '1 - '.(memory_get_usage(false)/1024/1024).'<br>';
+        if ($i % 50000 === 0) echo '1 - '.(memory_get_usage(false)/1024/1024).'<br>';
     }
 
     return $test_array;
@@ -30,8 +30,9 @@ function loop_value2(){
     $test_array = [];
     for ($i=0; $i <= 999999; $i++){
         yield $i;
-        if ($i % 20000 === 0) echo '2 - '.(memory_get_usage(false)/1024/1024).'<br>';
+        if ($i % 50000 === 0) echo '2 - '.(memory_get_usage(false)/1024/1024).'<br>';
     }
 }
+
 foreach(loop_value2() as $value){};
 ?>
